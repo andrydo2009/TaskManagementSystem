0aec5a5356ed0472ad82669d1bf8b9d24961e92a
andryDO2009 <andrydo2009@yandex.ru>
2023-12-07 00:41:12 +0300

    Creating a project, adding initial dependencies.

---
+++ src/test/java/com/example/system/management/task/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.system.management.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
